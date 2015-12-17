a8c5eb36f49dc247d9fb1d5d8dfb86c86df55583
Josh Harrison <joshharr1997@gmail.com>
2015-12-16 23:35:11 -0500

    v.01
    Location.Travel Function Created
    Created Classes Location, Character, Battle

---
+++ Text Based RPG/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Based_RPG
+{
+    class Location
+    {
+        public int playerX;
+        public int playerY;
+        public void Travel(int X, int Y)
+        {
+            playerX = X;
+            playerY = Y;
+        }
+    }
+}
